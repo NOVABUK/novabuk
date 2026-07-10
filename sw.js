813335a2bdf2ca903e9ec6d657881fa566e6aeab
Logsols <nelsonjeremiah699@gmail.com>
2026-07-10 10:56:20 +0100

    google fixed

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "/novabuk-static-v1";
+const CACHE_NAME = "/novabuk-static-v7";
 const OFFLINE_PAGE = "/offline.html";
 const CLINIC_OFFLINE_PAGE = "/clinic-offline.html";
 const CLINIC_QUEUE_PAGE = "/clinic-queue.html";
