14c1ef2dc9942140c9ee7105af5e2e692da6d4d1
Logsols <nelsonjeremiah699@gmail.com>
2026-07-18 13:09:28 +0100

    notification changed

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "/novabuk-static-v11";
+const CACHE_NAME = "/novabuk-static-v12";
 const OFFLINE_PAGE = "/offline.html";
 const CLINIC_OFFLINE_PAGE = "/clinic-offline.html";
 const CLINIC_QUEUE_PAGE = "/clinic-queue.html";
