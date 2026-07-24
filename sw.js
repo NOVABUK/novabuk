2d5e4d8a5ecf89a1469ca72a5402e6a945cdcf5e
Logsols <nelsonjeremiah699@gmail.com>
2026-07-24 12:00:41 +0100

    billing added

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "/novabuk-static-v13";
+const CACHE_NAME = "/novabuk-static-v14";
 const OFFLINE_PAGE = "/offline.html";
 const CLINIC_OFFLINE_PAGE = "/clinic-offline.html";
 const CLINIC_QUEUE_PAGE = "/clinic-queue.html";
