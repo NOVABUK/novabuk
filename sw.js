a91f10dea2285b9f39de3d0598d9805a595aeaa6
Logsols <nelsonjeremiah699@gmail.com>
2026-08-23 01:52:23 +0100

    last commit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "/novabuk-static-v27";
+const CACHE_NAME = "/novabuk-static-v28";
 const OFFLINE_PAGE = "/offline.html";
 const CLINIC_OFFLINE_PAGE = "/clinic-offline.html";
 const CLINIC_QUEUE_PAGE = "/clinic-queue.html";
